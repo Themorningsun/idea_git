fdfc299a4a9f464de4989e67f5d03b88fd811e63
lnh <lnh@163.com>
2018-12-07 21:43:27 +0800

    修改User类，增加number属性，增加set，get方法

--- git_project/src/main/java/com/lnh/domain/User.java
+++ git_project/src/main/java/com/lnh/domain/User.java
@@ -8,5 +8,29 @@ package com.lnh.domain;
 public class User {
     private Integer id;
     private String name;
+    private Integer number;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 }
